c5c591470b1c70ed070b01ede21de68ccb7d6e53
ashisnayak-18 <ashisranjannayak2002@gmail.com>
2025-05-09 00:34:54 +0530

    4rd commit

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -2,16 +2,20 @@ import { activityModel } from "../Models/activityModel.js";
 import { bookingModel } from "../Models/bookingModel.js";
 import { userModel } from "../Models/user.js";
 import catchAsyncError from "../Utils/catchAsyncError.js";
+import ErrorHandler from "../Utils/ErrorHandler.js";
 
 export const bookActivity = catchAsyncError(async (req, res, next) => {
   const { activityId } = req.params;
-  const { _id:userId } = req.user;
+  const { _id: userId } = req.user || {}; 
+  
 
-  if (!userId) {
+  const finalUserId = userId || req.body.userId;
+
+  if (!finalUserId) {
     return next(new ErrorHandler("User ID is required", 400));
   }
 
-  const user = await userModel.findById(userId);
+  const user = await userModel.findById(finalUserId);
   if (!user) {
     return next(new ErrorHandler("User not found", 404));
   }
@@ -21,19 +25,45 @@ export const bookActivity = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler("Activity not found", 404));
   }
 
-  const existingBooking = await bookingModel.findOne({ user: userId, activity: activityId });
-  if (existingBooking) {
-    return next(new ErrorHandler("Activity already booked by this user", 400));
+  // Prevent duplicate booking
+  const alreadyBooked = await bookingModel.findOne({
+    user: finalUserId,
+    activity: activityId,
+  });
+
+  if (alreadyBooked) {
+    return next(
+      new ErrorHandler("You have already booked this activity", 400)
+    );
   }
 
   const booking = await bookingModel.create({
-    user: userId,
+    user: finalUserId,
     activity: activityId,
   });
 
   res.status(201).json({
     success: true,
     message: "Activity booked successfully",
-    booking,
+    data: booking,
+  });
+});
+
+
+export const getAllActivities = catchAsyncError(async (req, res, next) => {
+  const activities = await activityModel
+    .find()
+    .populate("user")  
+    .populate("bookings") 
+    .exec();
+
+  if (!activities || activities.length === 0) {
+    return next(new ErrorHandler("No activities found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Activities fetched successfully",
+    data: activities,
   });
 });
--- Models/bookingModel.js
+++ Models/bookingModel.js
@@ -1,20 +1,23 @@
-import mongoose from "mongoose";
-
 const bookingSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
+    unique: true, // One booking doc per user
   },
-  activity: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Activity",
-    required: true,
-  },
-  bookedAt: {
-    type: Date,
-    default: Date.now,
-  },
+  activities: [
+    {
+      activity: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Activity",
+        required: true,
+      },
+      bookedAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
 });
 
 export const bookingModel = mongoose.model("Booking", bookingSchema);
